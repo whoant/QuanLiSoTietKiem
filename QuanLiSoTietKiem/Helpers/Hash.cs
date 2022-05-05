2bab54227e8afa7dbab034ed46321f5524944832
whoant <vovanhoangtuan4.2@gmail.com>
2022-05-05 18:31:04 +0700

    Create Helpers

---
+++ QuanLiSoTietKiem/Helpers/Hash.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace QuanLiSoTietKiem.helpers
+{
+    public static class Hash
+    {
+        public static string ComputeSha256(string text)
+        {
+            SHA256 sha256Hash = SHA256.Create();
+
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(text));
+
+            // Convert byte array to a string   
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+            return builder.ToString();
+
+        }
+    }
+}
