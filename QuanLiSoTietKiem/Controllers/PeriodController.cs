a836397b612e63c3fb54725a4fe9bfba9ced25aa
whoant <vovanhoangtuan4.2@gmail.com>
2022-05-12 17:09:41 +0700

    Period

--- QuanLiSoTietKiem/Controllers/PeriodController.cs
+++ QuanLiSoTietKiem/Controllers/PeriodController.cs
@@ -21,21 +21,7 @@ namespace QuanLiSoTietKiem.Controllers
             return View(db.Periods.ToList());
         }
 
-        // GET: Period/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Period period = db.Periods.Find(id);
-            if (period == null)
-            {
-                return HttpNotFound();
-            }
-            return View(period);
-        }
-
+    
         // GET: Period/Create
         public ActionResult Create()
         {
@@ -47,73 +33,21 @@ namespace QuanLiSoTietKiem.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Name,Month,CreatedAt,UpdatedAt")] Period period)
+        public ActionResult Create([Bind(Include = "ID,Name,Month")] Period period)
         {
             if (ModelState.IsValid)
             {
-                db.Periods.Add(period);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                if (db.Periods.Count(p => p.Month == period.Month) == 0)
+                {
+                    db.Periods.Add(period);
+                    db.SaveChanges();
+                    TempData["Error"] = "Tạo thành công !";
+                    return RedirectToAction("Index");
+                }
+                TempData["Error"] = "Kì hạn này đã tồn tại !";
             }
 
             return View(period);
-        }
-
-        // GET: Period/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Period period = db.Periods.Find(id);
-            if (period == null)
-            {
-                return HttpNotFound();
-            }
-            return View(period);
-        }
-
-        // POST: Period/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Name,Month,CreatedAt,UpdatedAt")] Period period)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(period).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(period);
-        }
-
-        // GET: Period/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Period period = db.Periods.Find(id);
-            if (period == null)
-            {
-                return HttpNotFound();
-            }
-            return View(period);
-        }
-
-        // POST: Period/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Period period = db.Periods.Find(id);
-            db.Periods.Remove(period);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
